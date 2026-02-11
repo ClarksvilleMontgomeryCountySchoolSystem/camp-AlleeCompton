0c6bdf9bdee65b081aeb2be89294be1c06d292b1
AlleeCompton <allee.compton456@students.cmcss.net>
2026-02-11 11:42:20 -0600

    Camp assignment

--- task2.py
+++ task2.py
@@ -2,39 +2,38 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 # For each variable convert to the appropriate data type.
 
-
 # Not all variables will need conversion.
 
 
 # first
-
+print(first)
 
 # last
-
+print(last)
 
 # birth
-
-
+print(birth)
+birth = int(birth)
 # days
-
-
+print(days)
+days = int(days)
 # p_first
-
+print (p_first)
 
 # p_last
-
+print(p_last)
 
 # phone
-
+print(phone)
 
 # street
-
+print(street)
 
 # city
-
+print(city)
 
 # state
-
+print(state)
 
 # zip_code
-
+print(zip_code)
